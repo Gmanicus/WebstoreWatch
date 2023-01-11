37f4cc77f635578d511f9718612b8e76dcf6d520
Grant <nerfboyforever@gmail.com>
2023-01-10 22:43:53 -0600

    Added error/special status handling

--- client.js
+++ client.js
@@ -4,6 +4,8 @@ let form2;
 let form3;
 let form4;
 let form5;
+let form6;
+let form7;
 let selectedExtension;
 
 const SERVER_IP = "http://localhost:5000";
@@ -15,6 +17,8 @@ window.onload = () => {
     form3 = document.getElementById("form-3");
     form4 = document.getElementById("form-4");
     form5 = document.getElementById("form-5");
+    form6 = document.getElementById("form-6");
+    form7 = document.getElementById("form-7");
 }
 
 async function getExtension() {
@@ -25,26 +29,28 @@ async function getExtension() {
     loader.classList.remove("hidden");
     
     
-    let responseData = await fetch(`${SERVER_IP}/extension`, {
+    let response = await fetch(`${SERVER_IP}/extension`, {
         method: 'POST',
         mode: "cors",
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({"ext-id": id})
-    }).then(response => {
-        if (response.ok) return response.json();
-        else return false;
     }).catch(() => false)
 
-    let data = await responseData;
-    console.log(data);
-    if (!data) {
+    // Throw bad request
+    if (response.status == 400) {
+        loader.classList.add("hidden");
+        form7.classList.remove("hidden");
+        return;
+    // Throw error
+    } else if (!response.ok) {
         loader.classList.add("hidden");
         form5.classList.remove("hidden");
         return;
     }
     
+    let data = await response.json();
     
     selectedExtension = data;
     selectedExtension.id = id;
@@ -85,7 +91,7 @@ async function submitWatcher() {
     form3.classList.add("hidden");
     loader.classList.remove("hidden");
 
-    let success = await fetch(`${SERVER_IP}/watch`, {
+    let response = await fetch(`${SERVER_IP}/watch`, {
         method: 'POST',
         mode: "cors",
         headers: {
@@ -96,13 +102,11 @@ async function submitWatcher() {
             "ext-id": selectedExtension.id,
             "ext-name": selectedExtension.name
         })
-    }).then(response => {
-        if (response.ok) return true;
-        else return false;
     }).catch(() => false)
     
     loader.classList.add("hidden");
 
-    if (success) form4.classList.remove("hidden");
-    else         form5.classList.remove("hidden");
+    if (response?.ok && response?.status != 418) form4.classList.remove("hidden");
+    else if (response?.status == 418)            form6.classList.remove("hidden");
+    else                                         form5.classList.remove("hidden");
 }
