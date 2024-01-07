66167c0021e56149c6e3881e236375e1e7a3dbf0
Grant Scrits <11013079+Gmanicus@users.noreply.github.com>
2024-01-07 15:57:07 -0600

    Update API url

--- client.js
+++ client.js
@@ -8,7 +8,7 @@ let form6;
 let form7;
 let selectedExtension;
 
-const SERVER_IP = "https://WebstoreWatcher.gmanicus.repl.co";
+const SERVER_IP = "https://webstore-watcher-Gmanicus.replit.app";
 
 window.onload = () => {
     loader = document.getElementById("loading-icon");
